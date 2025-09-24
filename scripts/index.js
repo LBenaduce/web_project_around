08a9770f0c34f4fe47d3455bb991f67486f28a46
Luigi <luigibenaduce@proton.me>
2025-09-24 17:21:57 -0300

    popup3

--- scripts/index.js
+++ scripts/index.js
@@ -1,171 +1,36 @@
-import { Card } from './Card.js';
-import { FormValidator } from './FormValidator.js';
-import { Section } from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import PopupWithConfirmation from './PopupWithConfirmation.js';
-import { UserInfo } from './UserInfo.js';
-import ModalProfile from './ModalProfile.js';
-import Api from './Api.js';
-import logoSrc from '../images/logo.png';
-import profileImageSrc from '../images/profile__image.jpg';
-import addIconSrc from '../images/add.svg';
-import editIconSrc from '../images/edit.svg';
-
-let userId;
-
-const profileImage = document.querySelector('.profile__image');
-const headerLogo = document.querySelector('.header__logo');
 const profileEditButton = document.querySelector('.profile__edit-button');
-const profileAddButton = document.querySelector('.profile__add-button');
-const popupProfileImage = document.querySelector('.profile__image-button');
+const editPopup = document.getElementById('edit-popup');
+const closeButton = editPopup.querySelector('.popup__close');
+const nameInput = editPopup.querySelector('input[name="name"]');
+const aboutInput = editPopup.querySelector('input[name="about"]');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const form = editPopup.querySelector('.popup__form');
 
-profileImage.src = profileImageSrc;
-headerLogo.src = logoSrc;
-profileAddButton.style.backgroundImage = `url(${addIconSrc})`;
-profileEditButton.style.backgroundImage = `url(${editIconSrc})`;
-
-const api = new Api({
-  baseUrl: "https://around.nomoreparties.co/v1/web-ptbr-cohort-12",
-  headers: {
-    authorization: "c27e8b71-839c-45ed-b81c-981deeb16426",
-    "Content-Type": "application/json"
-  }
-});
-
-function handleLikeClick(cardId, isLiked) {
-  return isLiked ? api.unlikeCard(cardId) : api.likeCard(cardId);
+function openPopup() {
+  editPopup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileJob.textContent;
 }
 
-const deletePopup = new PopupWithConfirmation({
-  popupSelector: '#popup_type_delete-card',
-  handleFormSubmit: (card) => {
-    api.deleteCard(card._id)
-      .then(() => {
-        card._element.remove();
-        deletePopup.close();
-      })
-      .catch((err) => console.error('Erro ao excluir o cartão:', err));
-  }
-});
-deletePopup.setEventListeners();
-
-function createCard(data) {
-  const card = new Card(
-    {
-      name: data.name,
-      link: data.link,
-      likes: data.likes,
-      id: data._id,
-      userId,
-      ownerId: data.owner._id
-    },
-    '#template-card',
-    ({ name, link }) => handleCardClick({ name, link }),
-    (cardId, isLiked, card) => handleLikeClick(cardId, isLiked, card),
-    () => deletePopup.open(card)
-  );
-  return card.generateCard();
+function closePopup() {
+  editPopup.classList.remove('popup_opened');
 }
 
-const cardList = new Section({
-  items: [],
-  renderer: (item) => {
-    const cardElement = createCard(item);
-    cardList.addItem(cardElement);
-  }
-}, '.cards__list');
+profileEditButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 
-api.getInitialCards()
-  .then((cards) => {
-    if (Array.isArray(cards)) {
-      cardList.renderItems(cards);
-    } else {
-      console.error("Erro: O retorno da API não é um array", cards);
-    }
-  })
-  .catch((err) => console.log("Erro ao carregar os cartões:", err));
-
-const userInfo = new UserInfo({
-  nameSelector: '.profile__name',
-  jobSelector: '.profile__job'
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = aboutInput.value;
+  closePopup();
 });
 
-api.getUserInfo()
-  .then((result) => {
-    userId = result._id;
-    userInfo.setUserInfo({
-      name: result.name,
-      job: result.about
-    });
-    profileImage.src = result.avatar;
-  })
-  .catch((err) => console.log("Erro ao carregar as informações do usuário:", err));
-
-function handleCardClick({ name, link }) {
-  imagePopup.open({ name, link });
-}
-
-const imagePopup = new PopupWithImage('.modalImage');
-imagePopup.setEventListeners();
-
-const addCardPopup = new PopupWithForm({
-  popupSelector: '#popup-card',
-  handleFormSubmit: (formData) => {
-    api.addCard({
-      name: formData['popup-card-name'],
-      link: formData['popup-card-link']
-    })
-      .then((newCard) => {
-        const cardElement = createCard(newCard);
-        cardList.addItem(cardElement);
-        addCardPopup.close();
-      })
-      .catch(err => console.log("Erro ao adicionar um novo cartão:", err));
-  }
-});
-addCardPopup.setEventListeners();
-
-const editProfilePopup = new PopupWithForm({
-  popupSelector: '#edit-popup',
-  handleFormSubmit: (data) => {
-    api.updateUserInfo({
-      name: data.name,
-      about: data.about
-    })
-      .then(() => {
-        userInfo.setUserInfo({ name: data.name, job: data.about });
-        editProfilePopup.close();
-      })
-      .catch(err => console.log("Erro ao atualizar nome", err));
-  }
-});
-editProfilePopup.setEventListeners();
-
-profileEditButton.addEventListener('click', () => {
-  const userInfoData = userInfo.getUserInfo();
-  document.querySelector('#profile-name-input').value = userInfoData.name;
-  document.querySelector('#profile-about-input').value = userInfoData.job;
-  editProfilePopup.open();
+editPopup.addEventListener('mousedown', (e) => {
+  if (e.target === editPopup) closePopup();
 });
 
-const modalProfile = new ModalProfile('#popup-profile-image');
-popupProfileImage?.addEventListener('click', () => modalProfile.open());
-
-profileAddButton.addEventListener('click', () => {
-  addCardPopup.open();
-});
-
-const validationSettings = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button-disable",
-  inputErrorClass: "popup__input-error",
-  errorClass: "popup__error_visible"
-};
-
-document.querySelectorAll(validationSettings.formSelector).forEach((formElement) => {
-  const formValidator = new FormValidator(validationSettings, formElement);
-  formValidator.enableValidation();
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') closePopup();
 });
