08abf9308e4a8053ee38086d0ad6cb2a143c93ce
LBenaduce <luiz.benaduce@gmail.com>
2025-12-28 18:30:40 -0300

    Fix Card.js duplication

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import { UserInfo } from "../components/UserInfo.js";
-import { PopupWithConfirmation } from "../components/PopupwithConfirmation.js";
-import { PopupWithForm } from "../components/PopupWithForm.js";
-import { PopupWithImage } from "../components/PopupWithImage.js";
-import { Section } from "../components/Section.js";
-import { Card } from "../components/Card.js";
-import { Api } from "../components/Api.js";
-import { FormValidator, validation } from "../components/formValidator.js";
-import { Utils } from "../components/utils.js";
+import { UserInfo } from "../scripts/UserInfo.js";
+import { PopupWithConfirmation } from "../scripts/PopupwithConfirmation.js";
+import { PopupWithForm } from "../scripts/PopupWithForm.js";
+import { PopupWithImage } from "../scripts/PopupWithImage.js";
+import { Section } from "../scripts/Section.js";
+import { Card } from "../scripts/Card.js";
+import { Api } from "../scripts/Api.js";
+import { FormValidator, validation } from "../scripts/formValidator.js";
+import { Utils } from "../scripts/utils.js";
 
 const utils = new Utils({
   form: document.querySelector(".popup__profile"),
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,72 +1,55 @@
-const POPUP_OPENED_CLASS = 'popup_opened';
+export class Utils {
+  constructor({
+    form,
+    title,
+    editname,
+    description,
+    about,
+    popup,
+    editprofile,
+    addpic,
+    photoAdd,
+  }) {
+    this._form = form;
+    this._title = title;
+    this._editname = editname;
+    this._description = description;
+    this._about = about;
+    this._popup = popup;
+    this._editprofile = editprofile;
+    this._addpic = addpic;
+    this._photoAdd = photoAdd;
 
-function handleEscClose(evt) {
-  if (evt.key === 'Escape') {
-    const opened = document.querySelector(`.${POPUP_OPENED_CLASS}`);
-    if (opened) {
-      closePopup(opened);
-    }
+    this.setEventListeners();
+  }
+
+  setEventListeners() {
+    this._form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      this._title.textContent = this._editname.value;
+      this._description.textContent = this._about.value;
+      this._popup.classList.add("popup-hidden");
+    });
+
+    this._editprofile.addEventListener("click", () => {
+      this._editname.value = this._title.textContent;
+      this._about.value = this._description.textContent;
+      this._popup.classList.remove("popup-hidden");
+      this._popup.classList.add("popup");
+    });
+
+    document.addEventListener("keydown", (evt) => {
+      if (evt.key === "Escape") {
+        document.querySelectorAll(".popup").forEach((popup) => {
+          if (!popup.classList.contains("popup-hidden")) {
+            popup.classList.add("popup-hidden");
+          }
+        });
+      }
+    });
+
+    this._addpic.addEventListener("click", () => {
+      this._photoAdd.classList.remove("popup-hidden");
+    });
   }
 }
-
-function openPopup(popup) {
-  popup.classList.remove('popup-hidden');
-  popup.classList.add(POPUP_OPENED_CLASS);
-  document.addEventListener('keydown', handleEscClose);
-}
-
-function closePopup(popup) {
-  popup.classList.remove(POPUP_OPENED_CLASS);
-  popup.classList.add('popup-hidden');
-
-  if (!document.querySelector(`.${POPUP_OPENED_CLASS}`)) {
-    document.removeEventListener('keydown', handleEscClose);
-  }
-}
-
-function setClosePopupByOverlay(popup) {
-  popup.addEventListener('mousedown', (evt) => {
-    if (
-      evt.target === popup ||
-      evt.target.classList.contains('popup__close') ||
-      evt.target.classList.contains('popup__close-zoom')
-    ) {
-      closePopup(popup);
-    }
-  });
-}
-
-export {
-  openPopup,
-  closePopup,
-  setClosePopupByOverlay
-};
-
-export const initialCards = [
-  {
-    name: 'Vale de Yosemite',
-    link: 'images/yosemite.png'
-  },
-
-  {
-    name: 'Lago Louise',
-    link: 'images/louise.png'
-  },
-  {
-    name: 'Montanhas Carecas',
-    link: 'images/montanhas.png'
-  },
-  {
-    name: 'Latemar',
-    link: 'images/latemar.png'
-  },
-  {
-    name: 'Parque Nacional de Vanoise',
-    link: 'images/vanoise.png'
-  },
-  {
-    name: 'Lago di Braies',
-    link: 'images/braies.png'
-  }
-];
-
