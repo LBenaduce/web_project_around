c267fdc0b5d9a1596a2deb5c7f64a851a3566f83
LBenaduce <luiz.benaduce@gmail.com>
2026-01-08 14:39:36 -0300

    New format2

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,34 @@ import Api from "../scripts/Api.js";
 
 const elementsSectionSelector = ".elements__list";
 
+const fallbackCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
+
 
 
 const profileNameElement = document.querySelector(".profile__name");
@@ -235,7 +263,11 @@ api
 
     avatarElement.src = userData.avatar;
 
-    cardSection.setItems(initialCards);
+    const cardsToRender = Array.isArray(initialCards) && initialCards.length > 1
+      ? initialCards
+      : fallbackCards;
+
+    cardSection.setItems(cardsToRender);
     cardSection.renderItems();
 
     console.log(
